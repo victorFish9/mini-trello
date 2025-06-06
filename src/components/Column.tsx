0f144fdc1e5178123b531dd1927133e1f21372c0
Victor <aue228lublupacanov@gmail.com>
2025-06-06 13:18:18 +0200

    column is now droppable but can't save tasks

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,6 +1,7 @@
 import type { FC } from "react"
 import type { Column as ColumnType, Task as TaskType } from "../types/types"
 import { Task } from "./Task"
+import { useDroppable } from "@dnd-kit/core"
 
 interface ColumnProps {
     column: ColumnType
@@ -8,16 +9,19 @@ interface ColumnProps {
 }
 
 export const Column: FC<ColumnProps> = ({ column, tasks }) => {
+    const { setNodeRef, isOver } = useDroppable({
+        id: column.id
+    })
+
+    const columnStyle = {
+        backgroundColor: isOver ? "#d2e3fc" : "#f4f5f7",
+        borderRadius: "6px",
+        width: "250px",
+        padding: "10px",
+        boxShadow: "0 2px 6px rgba(0,0,0,0.1)"
+    }
     return (
-        <div
-            style={{
-                backgroundColor: "#f4f5f7",
-                borderRadius: "6px",
-                width: "250px",
-                padding: "10px",
-                boxShadow: "0 2px 6px rgba(0,0,0,0.1)"
-            }}
-        >
+        <div ref={setNodeRef} style={columnStyle}>
             <h3 style={{ textAlign: "center", marginBottom: "10px" }}>{column.title}</h3>
             {tasks.map((task) => (
                 <Task key={task.id} task={task} />
